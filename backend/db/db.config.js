f960fc38e515a7e95244baad29adcb59f36aa1d9
keba <kebronluel813@gmail.com>
2026-07-03 07:11:40 -0700

    Refactor db.config.js for environment variables and SSL
    Updated database configuration to use environment variables for host and port, and added SSL configuration.

--- backend/db/db.config.js
+++ backend/db/db.config.js
@@ -1,16 +1,20 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
-
 
 export default db;
